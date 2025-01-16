daf9cec932064c6fcc9eed80bf15bf09847f7fcb
Samarjayee <samarjayee@gmail.com>
2025-01-16 17:37:02 +0000

    t-rex

--- app/components/TRexAnimation.tsx
+++ app/components/TRexAnimation.tsx
@@ -7,17 +7,14 @@ import gsap from 'gsap';
 export const TRexAnimation = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const trexRef = useRef<HTMLDivElement>(null);
-  const obstacleRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const tl = gsap.timeline({ repeat: 0 });
     
-    // Initial setup
     gsap.set(trexRef.current, { x: -100 });
     
-    // Animation sequence
     tl.to(trexRef.current, {
-      x: '120vw', // Move past the screen width
+      x: '120vw',
       duration: 4,
       ease: "none",
     })
@@ -27,41 +24,43 @@ export const TRexAnimation = () => {
       ease: "power1.out",
       repeat: 2,
       yoyo: true,
-      offset: 1.5 // Jump during the run
+      offset: 1.5
     });
 
-    // Cleanup
-    return () => {
-      tl.kill();
-    };
+    return () => tl.kill();
   }, []);
 
   return (
-    <div 
-      ref={containerRef} 
-      className="relative w-full h-24 mb-8 overflow-hidden"
-    >
+    <div ref={containerRef} className="relative w-full h-32 mb-8 overflow-hidden">
       {/* T-Rex */}
-      <div
-        ref={trexRef}
-        className="absolute bottom-0 w-12 h-16"
-      >
-        <div className="w-full h-full relative">
+      <div ref={trexRef} className="absolute bottom-0 w-24 h-24">
+        <div className="relative w-full h-full">
           {/* Body */}
-          <div className="absolute bottom-0 w-8 h-12 bg-white rounded-t-lg"></div>
+          <div className="absolute bottom-0 right-0 w-16 h-20 bg-white rounded-t-lg"></div>
+          
           {/* Head */}
-          <div className="absolute top-0 right-0 w-6 h-4 bg-white rounded-tr-lg"></div>
-          {/* Arm */}
-          <div className="absolute top-6 right-2 w-3 h-2 bg-white rounded"></div>
-          {/* Leg */}
-          <div className="absolute bottom-0 right-2 w-2 h-6 bg-white"></div>
+          <div className="absolute top-0 right-0 w-12 h-8 bg-white">
+            {/* Eye */}
+            <div className="absolute top-2 right-2 w-2 h-2 bg-black rounded-full"></div>
+            {/* Mouth */}
+            <div className="absolute bottom-0 right-0 w-8 h-3 bg-white">
+              <div className="absolute top-1 right-1 w-4 h-px bg-black"></div>
+            </div>
+          </div>
+          
+          {/* Arms */}
+          <div className="absolute top-8 right-6 w-4 h-3 bg-white"></div>
+          
+          {/* Legs */}
+          <div className="leg-back absolute bottom-0 right-2 w-3 h-8 bg-white animate-legMove"></div>
+          <div className="leg-front absolute bottom-0 right-10 w-3 h-8 bg-white animate-legMove delay-100"></div>
         </div>
       </div>
 
       {/* Obstacles */}
       <div className="absolute bottom-0 w-full">
-        <div ref={obstacleRef} className="absolute bottom-0 left-[40%] w-4 h-8 bg-white"></div>
-        <div className="absolute bottom-0 left-[60%] w-4 h-6 bg-white"></div>
+        <div className="absolute bottom-0 left-[40%] w-4 h-12 bg-white"></div>
+        <div className="absolute bottom-0 left-[60%] w-4 h-10 bg-white"></div>
       </div>
 
       {/* Ground line */}
