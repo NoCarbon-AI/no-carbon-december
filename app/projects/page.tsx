97363f574e3ce37c87d1ab72e1daef4280d83f08
Samarjayee <samarjayee@gmail.com>
2025-01-23 23:29:02 +0000

    d

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -109,8 +109,8 @@ export default async function ProjectsPage() {
     src="/Footer-image.png"
     alt="Footer"
     width={1920}
-    height={150} // Set to desired max height
-    className="w-full h-[150px]" // Fixed height of 30px
+    height={100} // Set to desired max height
+    className="w-full h-[100px]" // Fixed height of 30px
     style={{
       position: 'absolute',
       bottom: 0,
