0d32dbe7431d4b9d4f90990bd6c4dcb4fa1338c7
Samarjayee <samarjayee@gmail.com>
2025-02-06 18:14:09 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,21 +2,15 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		mdxRs: true,
-	},
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true,
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 };
 
 export default withContentlayer(nextConfig);
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	images: {
-	  dangerouslyAllowSVG: true,
-	  contentDispositionType: 'attachment',
-	  contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-	},
-  }
-  
-  export default nextConfig;
