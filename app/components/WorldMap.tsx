f240f4bc2ed4aa40721aea72f764bf3587c1b1a7
Samarjayee <samarjayee@gmail.com>
2025-02-13 13:16:57 +0000

    Update WorldMap.tsx

--- app/components/WorldMap.tsx
+++ app/components/WorldMap.tsx
@@ -9,7 +9,6 @@ import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 // Fix the default icon issue
-delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
     iconUrl: markerIcon.src,
     iconRetinaUrl: markerIcon2x.src,
