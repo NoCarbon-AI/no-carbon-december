025e630b3d57d2c4e408df7107bbc62f0362e9bf
Samarjayee <samarjayee@gmail.com>
2025-02-15 17:06:49 +0000

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -58,7 +58,7 @@ export default async function ProjectsPage() {
     );
 
   return (
-    <div className="relative pb-16">
+    <div className="relative min-h-screen">
       <Navigation />
       <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
         <div className="relative">
@@ -113,7 +113,8 @@ export default async function ProjectsPage() {
 
       </div>
 {/* Footer */}
-<Footer />
-    </div>
+<div className="w-full absolute bottom-0">
+  <Footer />
+</div>
   );
 }
