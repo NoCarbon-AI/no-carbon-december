63089f8ab9ab98a758301a1889716f866bb12868
Samarjayee <samarjayee@gmail.com>
2025-02-15 11:55:40 +0000

    Services
    clickbale

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -111,12 +111,7 @@ export const Navigation: React.FC = () => {
               <Link
     href="/projects"  // Add this Link wrapper
     className="duration-200 text-zinc-400 hover:text-zinc-100"
-  ></Link>
-              <button
-                className="duration-200 text-zinc-400 hover:text-zinc-100"
-              >
-                Services
-              </button>
+></Link>
 
               {showSubmenu && (
                 <div 
